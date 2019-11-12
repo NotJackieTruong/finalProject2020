2857a39620e386ff7b045d68aca4e1a9cfbe8ad1
khanh <khanhtq.b8084@st.usth.edu.vn>
2019-11-12 11:29:41 +0700

    add navbar

--- public/javascripts/gmap.js
+++ public/javascripts/gmap.js
@@ -1,22 +1,22 @@
 function initMap(){
-    console.log("This file works")
+    console.log("This file works and is from gmap.js")
     var uluru ={lat: 21.0453913, lng:105.8172996}
-    var src=import('../kmlFiles/HaNoi.kml')
+    // var src=import('../kmlFiles/HaNoi.kml')
     var map = new google.maps.Map(
         document.getElementById('map'), {
             zoom:6,
             center: uluru
         })
-    var haNoiLayer = new google.maps.KmlLayer(src,{
-        suppressInfoWindows: true,
-        preserveViewport: false,
-        map:map
-    })
-    haNoiLayer.addListener('click', function(event){
-        var content=event.featureData.infoWIndowHtml;
-        var testimonial = document.getElementById('capture');
-        testimonial.innerHTML=content;
-    })
+    // var haNoiLayer = new google.maps.KmlLayer(src,{
+    //     suppressInfoWindows: true,
+    //     preserveViewport: false,
+    //     map:map
+    // })
+    // haNoiLayer.addListener('click', function(event){
+    //     var content=event.featureData.infoWIndowHtml;
+    //     var testimonial = document.getElementById('capture');
+    //     testimonial.innerHTML=content;
+    // })
     var marker = new google.maps.Marker({
         position:uluru, 
         title: "uluru",
