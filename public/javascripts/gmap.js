beaf23741620fcac3b1924d555bf9bc89a966118
Ngoc Do <hung8585@Hung8585s-MacBook.local>
2019-11-25 12:50:20 +0700

    25.11.19 Done Visualising Data #1
    - HaNoi Data only
    - Haven't applied JSON

--- public/javascripts/gmap.js
+++ public/javascripts/gmap.js
@@ -1,75 +1,31 @@
-//function ve polygon
-function drawPolygon(googlemap){
-    var phucxaPolygon = new google.maps.Polygon({
-        paths: arrCoor,
-        strokeColor: 'blue',
-        strokeOpacity: 0.2,
-        strokeWeight: 3,
-        fillColor: 'blue',
-        fillOpacity: 0.35
-    });
-    phucxaPolygon.setMap(googlemap);
+var maxPopulation = 58554
+var minPopulation = 1410
+
+function opacityOverlay(population){
+    var opacity = (population - minPopulation)/(maxPopulation-minPopulation)
+    return opacity;
 }
+// function ve polygon
+function drawPolygon(googlemap, Pathcoordinate, id){
+    var setPolygon = new google.maps.Polygon({
+        paths: Pathcoordinate,
+        strokeColor: 'white',
+        strokeOpacity: 1,
+        strokeWeight: 0.1,
+        fillColor: 'red',
+        fillOpacity: opacityOverlay(ObjectData[id].Population)
+    });
+    setPolygon.setMap(googlemap);
+}
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 21.0453913, lng:105.8172996},
+        zoom: 9
+    });
 
-function initMap(){
-    console.log("This file works and is from gmap.js")
-    var center ={lat: 21.0453913, lng:105.8172996}
-    var src='https://raw.githubusercontent.com/NotJackieTruong/finalProject2020/master/kmlFiles/HaNoi.kml'
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom:6,
-            center: center
-        })
-    // var haNoiLayer = new google.maps.KmlLayer(src,{
-    //     suppressInfoWindows: true,
-    //     preserveViewport: false,
-    //     map:map
-    // })
-    // haNoiLayer.addListener('click', function(event) {
-    //     var content = event.featureData.description;
-    //     var testimonial = document.getElementById('capture');
-    //     testimonial.innerHTML = content;
-    //   });
-
-    // test function draw
-    drawPolygon(map)
-    
-    // test pop up thong tin
-    // var markers = [];
-    // var geocoder = new google.maps.Geocoder;
-    // var infowindow = new google.maps.InfoWindow();
-
-    // map.addListener("click", function(e) {
-    //     for (var i = 0; i < markers.length; i++) {
-    //         markers[i].setMap(null)
-    //     }
-    //     markers = [];
-
-    //     geocoder.geocode(
-    //         { "location": e.latLng },
-    //         function(results, status) {
-    //             if (status === google.maps.GeocoderStatus.OK) {
-    //             if (results[0]) {
-    //                 infowindow.setContent(
-    //                 "<div>" +
-    //                     "<b>Address :</b> " + results[0].formatted_address + "<br>" +
-    //                     "<b>Latitude :</b> " + results[0].geometry.location.lat() + "<br>" +
-    //                     "<b>Longitude :</b> " + results[0].geometry.location.lng() +
-    //                 "</div>"
-    //                 );
-    //                 infowindow.open(map, marker);
-    //             } else {
-    //                 console.log("No results found");
-    //             }
-    //             } else {
-    //             console.log("Geocoder failed due to: " + status);
-    //             }
-    //         }
-    //         );
-
-    //         map.panTo(marker.position); // Set new point to center of map
-
-    //         markers.push(marker); // add new marker to markers array
-    //     });
-  
+    //ve polygon theo id cua ObjectData
+    for(var i = 0 ; i <  ObjectData.length; i++){
+        drawPolygon(map, getCoordinate(i), i)
+    }
 }
--- public/javascripts/testfunction.js
+++ public/javascripts/testfunction.js
@@ -1,52 +1,23 @@
-// var id = ??
-// id dung chung cho arrInfor va arrCoordinate
 
-//thu tao array chua nhieu array
-var arrAll = []
-var arrAlter = []
-var m = 0
-var arrInfor = []
-var maxPopulation = 10000
-var minPopulation = 5000
+var maxPopulation = 58554
+var minPopulation = 1410
 
-// luu cac arrCoordinate thanh 1 arrAll dung cho for loop
-for(var j = 0; j < 20; j++){
-    for( var i = 0; i < arr.length; ){
-        storeCoordinate((arr[i]+m),(arr[i+1]+m),arrAlter);
-        i  = i+2
-    }
-    m+=0.008
-    arrAll.push({array: arrAlter,id: j})
-    arrAlter = []
-}
-
-//function tinh color opacity cua 1 area 
 function opacityOverlay(population){
     var opacity = (population - minPopulation)/(maxPopulation-minPopulation)
     return opacity;
 }
-
-// test array infor, object format: { danso, opacity, id}
-var n = 5000
-for(var j = 0; j < 20; j++){
-        arrInfor.push({population: n, opaR: opacityOverlay(n), id: j})
-        n = n+250
-}
-console.log(arrInfor)
-
 // function ve polygon
-function drawPolygon(googlemap, arrCoordinate, i){
-    var phucxaPolygon = new google.maps.Polygon({
-        paths: arrCoordinate,
+function drawPolygon(googlemap, Pathcoordinate, id){
+    var setPolygon = new google.maps.Polygon({
+        paths: Pathcoordinate,
         strokeColor: 'red',
-        strokeOpacity: 0,
-        strokeWeight: 3,
+        strokeOpacity: 1,
+        strokeWeight: 0.1,
         fillColor: 'blue',
-        fillOpacity: arrInfor[i].opaR
+        fillOpacity: opacityOverlay(ObjectData[id].Population)
     });
-    phucxaPolygon.setMap(googlemap);
+    setPolygon.setMap(googlemap);
 }
-
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -54,10 +25,8 @@ function initMap() {
         zoom: 12
     });
 
-    // ve cac polygon cho tat ca cac arrCoordinate co trong arrAll
-    for(var i = 0 ;  i<arrAll.length; i++){
-        drawPolygon(map,arrAll[i].array,i)
-        console.log(arrAll[i].id)
+    //ve polygon theo id cua ObjectData
+    for(var i = 0 ; i <  ObjectData.length; i++){
+        drawPolygon(map, getCoordinate(i), i)
     }
-    
 }
