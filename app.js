a56e283f36f468193c251e2aa46fb16623081157
Ngoc Do <hung8585@Hung8585s-MacBook.local>
2019-12-14 03:02:20 +0700

    update

--- app.js
+++ app.js
@@ -42,8 +42,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, '0.0.0.0', function() {
-  console.log('Listening to port:  ' + 3000);
-});
-
 module.exports = app;
